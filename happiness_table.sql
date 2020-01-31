2e077b270234ac0c7458de5c88d0fb8a3af77526
daluongo <desiree.luongo@gmail.com>
2020-01-30 19:29:08 -0500

    folders for structure and app.py template from pet_pals with some changes

--- happiness_table.sql
+++ happiness_table.sql
@@ -1,68 +1,68 @@
-CREATE TABLE data_2015 (
-	country VARCHAR(40)             NOT NULL,
-  	happiness_rank FLOAT            NOT NULL,
-  	happiness_score FLOAT           NOT NULL,
-  	gdp_per_capita FLOAT,
-  	social_support FLOAT,
-  	life_expectancy FLOAT,
-  	freedom FLOAT,
-  	generosity FLOAT,
- 	government_corr FLOAT,
- 	PRIMARY KEY (country)
- );
+-- CREATE TABLE data_2015 (
+-- 	country VARCHAR(40)             NOT NULL,
+--   	happiness_rank FLOAT            NOT NULL,
+--   	happiness_score FLOAT           NOT NULL,
+--   	gdp_per_capita FLOAT,
+--   	social_support FLOAT,
+--   	life_expectancy FLOAT,
+--   	freedom FLOAT,
+--   	generosity FLOAT,
+--  	government_corr FLOAT,
+--  	PRIMARY KEY (country)
+--  );
 
-CREATE TABLE data_2016(
-	country VARCHAR(40)             NOT NULL,
-  	happiness_rank FLOAT            NOT NULL,
-  	happiness_score FLOAT           NOT NULL,
-  	gdp_per_capita FLOAT,
-  	social_support FLOAT,
-  	life_expectancy FLOAT,
-  	freedom FLOAT,
-  	generosity FLOAT,
- 	government_corr FLOAT,
- 	PRIMARY KEY (country)
- );
+-- CREATE TABLE data_2016(
+-- 	country VARCHAR(40)             NOT NULL,
+--   	happiness_rank FLOAT            NOT NULL,
+--   	happiness_score FLOAT           NOT NULL,
+--   	gdp_per_capita FLOAT,
+--   	social_support FLOAT,
+--   	life_expectancy FLOAT,
+--   	freedom FLOAT,
+--   	generosity FLOAT,
+--  	government_corr FLOAT,
+--  	PRIMARY KEY (country)
+--  );
 
-CREATE TABLE data_2017(
-	country VARCHAR(40)             NOT NULL,
-  	happiness_rank FLOAT            NOT NULL,
-  	happiness_score FLOAT           NOT NULL,
-  	gdp_per_capita FLOAT,
-  	social_support FLOAT,
-  	life_expectancy FLOAT,
-  	freedom FLOAT,
-  	generosity FLOAT,
- 	government_corr FLOAT,
- 	PRIMARY KEY (country)
- );
+-- CREATE TABLE data_2017(
+-- 	country VARCHAR(40)             NOT NULL,
+--   	happiness_rank FLOAT            NOT NULL,
+--   	happiness_score FLOAT           NOT NULL,
+--   	gdp_per_capita FLOAT,
+--   	social_support FLOAT,
+--   	life_expectancy FLOAT,
+--   	freedom FLOAT,
+--   	generosity FLOAT,
+--  	government_corr FLOAT,
+--  	PRIMARY KEY (country)
+--  );
  
-CREATE TABLE data_2018(
-	country VARCHAR(40)             NOT NULL,
-  	happiness_rank FLOAT            NOT NULL,
-  	happiness_score FLOAT           NOT NULL,
-  	gdp_per_capita FLOAT,
-  	social_support FLOAT,
-  	life_expectancy FLOAT,
-  	freedom FLOAT,
-  	generosity FLOAT,
- 	government_corr FLOAT,
- 	PRIMARY KEY (country)
- );
+-- CREATE TABLE data_2018(
+-- 	country VARCHAR(40)             NOT NULL,
+--   	happiness_rank FLOAT            NOT NULL,
+--   	happiness_score FLOAT           NOT NULL,
+--   	gdp_per_capita FLOAT,
+--   	social_support FLOAT,
+--   	life_expectancy FLOAT,
+--   	freedom FLOAT,
+--   	generosity FLOAT,
+--  	government_corr FLOAT,
+--  	PRIMARY KEY (country)
+--  );
 
-CREATE TABLE data_2019(
-  	country VARCHAR(40)             NOT NULL,
-  	happiness_rank FLOAT            NOT NULL,
-  	happiness_score FLOAT           NOT NULL,
-  	gdp_per_capita FLOAT,
-  	social_support FLOAT,
-  	life_expectancy FLOAT,
-  	freedom FLOAT,
-  	generosity FLOAT,
- 	government_corr FLOAT,
- 	PRIMARY KEY (country)
- );
+-- CREATE TABLE data_2019(
+--   	country VARCHAR(40)             NOT NULL,
+--   	happiness_rank FLOAT            NOT NULL,
+--   	happiness_score FLOAT           NOT NULL,
+--   	gdp_per_capita FLOAT,
+--   	social_support FLOAT,
+--   	life_expectancy FLOAT,
+--   	freedom FLOAT,
+--   	generosity FLOAT,
+--  	government_corr FLOAT,
+--  	PRIMARY KEY (country)
+--  );
 
-SELECT * FROM data_2019
+-- SELECT * FROM data_2019
 
 
