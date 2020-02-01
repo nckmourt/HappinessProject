60ae3d32e4e5b1af552bdccc88cd0ce1f2404582
Robel Wondimu <rbwondimu@gmail.com>
2020-02-01 13:13:56 -0500

    commit

--- Happiness_map/static/js/config.js
+++ Happiness_map/static/js/config.js
@@ -1,2 +1,2 @@
 // API key
-const API_KEY ="pk.eyJ1IjoibWR1cnN0MTIiLCJhIjoiY2p5MGRqaGwxMDEwdjNkbnFieGp0dHhkMyJ9.4Iq_jyHDh4ZhNYz8EckjBQ"
+const API_KEY ="pk.eyJ1Ijoicm9iZWx3b24iLCJhIjoiY2s1aGU5Mzc2MDJ4NDNkcDdxMng2ajA4YSJ9.Ojwn4foHr3H5Y1G8iui6cA"
