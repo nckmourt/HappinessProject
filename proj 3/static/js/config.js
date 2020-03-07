a6e3a328d7e121590544e40ad0c94e0cc08ab047
daluongo <desiree.luongo@gmail.com>
2020-03-07 10:30:01 -0500

    changing deploy branch

--- proj 3/static/js/config.js
+++
@@ -1,3 +0,0 @@
-// API key
-// const API_KEY ="pk.eyJ1Ijoicm9iZWx3b24iLCJhIjoiY2s1aGU5Mzc2MDJ4NDNkcDdxMng2ajA4YSJ9.Ojwn4foHr3H5Y1G8iui6cA"
-const API_KEY ="pk.eyJ1Ijoicm9iZWx3b24iLCJhIjoiY2s1aGU5Mzc2MDJ4NDNkcDdxMng2ajA4YSJ9.Ojwn4foHr3H5Y1G8iui6cA"
