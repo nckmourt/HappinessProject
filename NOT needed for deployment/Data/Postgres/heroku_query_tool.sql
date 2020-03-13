a5c4f6ee1e9fd23ec301edb9301b43b30d2e14a1
daluongo <desiree.luongo@gmail.com>
2020-03-13 08:29:09 -0400

    added all excel data, originial and clean and postgres code

--- NOT needed for deployment/Data/Postgres/heroku_query_tool.sql
+++ NOT needed for deployment/Data/Postgres/heroku_query_tool.sql
@@ -10,43 +10,12 @@ CREATE TABLE data_2015 (
  	government_corr FLOAT,
 	social_support FLOAT,
 	continent VARCHAR(40)           NOT NULL,
-	population FLOAT                NOT NULL,
- 	PRIMARY KEY (country)
- );
-
-CREATE TABLE data_2016(
-	country VARCHAR(40)             NOT NULL,
-  	happiness_rank FLOAT            NOT NULL,
-  	happiness_score FLOAT           NOT NULL,
-  	gdp_per_capita FLOAT,
-  	family FLOAT,
-  	life_expectancy FLOAT,
-  	freedom FLOAT,
-  	generosity FLOAT,
- 	government_corr FLOAT,
-	social_support FLOAT,
-	continent VARCHAR(40)           NOT NULL,
-	population FLOAT                NOT NULL,
- 	PRIMARY KEY (country)
- );
-
-CREATE TABLE data_2017(
-	country VARCHAR(40)             NOT NULL,
-  	happiness_rank FLOAT            NOT NULL,
-  	happiness_score FLOAT           NOT NULL,
-  	gdp_per_capita FLOAT,
-  	family FLOAT,
-  	life_expectancy FLOAT,
-  	freedom FLOAT,
-  	generosity FLOAT,
- 	government_corr FLOAT,
-	social_support FLOAT,
-	continent VARCHAR(40)           NOT NULL,
-	population FLOAT                NOT NULL,
+	population FLOAT,
+	gdp_usd MONEY,
  	PRIMARY KEY (country)
  );
  
-CREATE TABLE data_2018(
+ CREATE TABLE data_2016 (
 	country VARCHAR(40)             NOT NULL,
   	happiness_rank FLOAT            NOT NULL,
   	happiness_score FLOAT           NOT NULL,
@@ -58,11 +27,12 @@ CREATE TABLE data_2018(
  	government_corr FLOAT,
 	social_support FLOAT,
 	continent VARCHAR(40)           NOT NULL,
-	population FLOAT                NOT NULL,
+	population FLOAT,
+	gdp_usd MONEY,
  	PRIMARY KEY (country)
  );
-
-CREATE TABLE data_2019(
+ 
+ CREATE TABLE data_2017 (
 	country VARCHAR(40)             NOT NULL,
   	happiness_rank FLOAT            NOT NULL,
   	happiness_score FLOAT           NOT NULL,
@@ -74,8 +44,44 @@ CREATE TABLE data_2019(
  	government_corr FLOAT,
 	social_support FLOAT,
 	continent VARCHAR(40)           NOT NULL,
-	population FLOAT                NOT NULL,
+	population FLOAT,
+	gdp_usd MONEY,
  	PRIMARY KEY (country)
  );
+ 
+ CREATE TABLE data_2018 (
+	country VARCHAR(40)             NOT NULL,
+  	happiness_rank FLOAT            NOT NULL,
+  	happiness_score FLOAT           NOT NULL,
+  	gdp_per_capita FLOAT,
+  	family FLOAT,
+  	life_expectancy FLOAT,
+  	freedom FLOAT,
+  	generosity FLOAT,
+ 	government_corr FLOAT,
+	social_support FLOAT,
+	continent VARCHAR(40)           NOT NULL,
+	population FLOAT,
+	gdp_usd MONEY,
+ 	PRIMARY KEY (country)
+ );
+ 
+ CREATE TABLE data_2019 (
+	country VARCHAR(40)             NOT NULL,
+  	happiness_rank FLOAT            NOT NULL,
+  	happiness_score FLOAT           NOT NULL,
+  	gdp_per_capita FLOAT,
+  	family FLOAT,
+  	life_expectancy FLOAT,
+  	freedom FLOAT,
+  	generosity FLOAT,
+ 	government_corr FLOAT,
+	social_support FLOAT,
+	continent VARCHAR(40)           NOT NULL,
+	population FLOAT,
+	gdp_usd MONEY,
+ 	PRIMARY KEY (country)
+ );
+ 
 
 
